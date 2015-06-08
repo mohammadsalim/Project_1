ef58ef6acfc43373caf0c26345409ca7b582aa50
Mohammad Salim <jd2474ev@gmail.com>
2015-06-08 12:25:30 -0400

    Polished CSS, in JS created variables for the game and a function to place values in the cells. However, the playerTurn variable is not working as it should

--- TTT/scripts/TTT.js
+++ TTT/scripts/TTT.js
@@ -4,4 +4,46 @@ var playerO = 'o';
 var playerTurn = 0;
 
 //Game Variables//
-var checkValue
+var checkValue;
+//Row A cell values//
+var a1;
+var a2;
+var a3;
+//Row B cell values//
+var b1;
+var b2;
+var b3;
+//Row C cell values//
+var c1;
+var c2;
+var c3;
+
+//Game Winner Variables//
+var checkWin;
+var xWin;
+var oWin;
+
+//Game Variables//
+var newGame;
+var boardRestart;
+
+//X or O placed in cells when selected//
+var newGame = function() {
+  $('td').on('click', function(event) {
+    if (playerTurn == 0) {
+      $(this).text(playerX);
+      checkValue();
+      checkWin();
+      playerTurn = 1;
+    } else {
+      $(this).text(playerO);
+      checkValue();
+      checkWin();
+      playerTurn = 0;
+    }
+  });
+};
+
+$(document).ready(function() {
+  newGame();
+}); //Used to start the newGame function//
