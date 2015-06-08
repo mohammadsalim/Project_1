c7aaed59327dce250ae6563e20347181aa5031c6
Mohammad Salim <jd2474ev@gmail.com>
2015-06-08 17:52:06 -0400

    created a checkWin function to assess the inputs of 'x' or 'o' and wheter they are 3-in-a-row or wheter it is a tie game

--- TTT/scripts/TTT.js
+++ TTT/scripts/TTT.js
@@ -59,14 +59,14 @@ var newGame = function() {
     if (playerTurn == 0 ) {
       $(this).text(playerX);
       $(this).off('click');
-      // checkValue();
-      // checkWin();
+      checkValue();
+      checkWin();
       playerTurn = 1;
     } else {
       $(this).text(playerO);
       $(this).off('click');
-      // checkValue();
-      // checkWin();
+      checkValue();
+      checkWin();
       playerTurn = 0;
     }
   });
@@ -93,14 +93,50 @@ checkValue = function () {
 };
 
 //Detect Win or Tie//
-//X
-checkWin = function() {
-  if ((a1 === a2 && a1 === a3 && (a1 = "x")) ||
-  (b1 === b2 && b1 === b3 && (b1 = "x")) ||
-  (c1 === c2 && c1 === c3 && (c1 = "x")) ||
-  (a1 === b1 && a1 === c1 && (a1 = "x")) ||
-  (a2 === b2 && a2 === c2 && (a2 = "x")) ||
-  (a3 === b3 && a3 === c3 && (a3 = "x"))))
+checkWin = function() { //X
+  if ((a1 == a2 && a1 == a3 && (a1 == "x")) ||
+  (b1 == b2 && b1 == b3 && (b1 == "x")) ||
+  (c1 == c2 && c1 == c3 && (c1 == "x")) ||
+  (a1 == b1 && a1 == c1 && (a1 == "x")) ||
+  (a2 == b2 && a2 == c2 && (a2 == "x")) ||
+  (a3 == b3 && a3 == c3 && (a3 == "x")) ||
+  (a1 == b2 && a1 == c3 && (a1 == "x")) ||
+  (a3 == b2 && a3 == c1 && (a3 == "x"))
+) {
+
+  xWin = true;
+  winnerIs();
+
+} else { //O
+  if ((a1 == a2 && a1 == a3 && (a1 == "o")) ||
+  (b1 == b2 && b1 == b3 && (b1 == "o")) ||
+  (c1 == c2 && c1 == c3 && (c1 == "o")) ||
+  (a1 == b1 && a1 == c1 && (a1 == "o")) ||
+  (a2 == b2 && a2 == c2 && (a2 == "o")) ||
+  (a3 == b3 && a3 == c3 && (a3 == "o")) ||
+  (a1 == b2 && a1 == c3 && (a1 == "o")) ||
+  (a3 == b2 && a3 == c1 && (a3 == "o"))
+) {
+
+  oWin = true;
+  winnerIs();
+
+} else { //Tie
+  if (((a1 == "x") || (a1 == "o")) &&
+  ((b1 == "x") || (b1 == "o")) &&
+  ((c1 == "x") || (c1 == "o")) &&
+  ((a2 == "x") || (a2 == "o")) &&
+  ((b2 == "x") || (b2 == "o")) &&
+  ((c2 == "x") || (c2 == "o")) &&
+  ((a3 == "x") || (a3 == "o")) &&
+  ((b3 == "x") || (b3 == "o")) &&
+  ((c3 == "x") || (c3 == "o"))
+  ) {
+      alert("It's a draw!")
+    }
+   }
+  }
 };
 
-//O
+//Declares who the winner is//
+var winnerIs 
